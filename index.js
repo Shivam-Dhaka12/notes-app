d0b91cf56c56c6619f592bf68440c3717fa347f4
Shivam Dhaka <shivamdhaka1200@gmail.com>
2023-12-15 13:14:35 +0530

    Deployment Testing -4: set views to /views

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.get('/home', (req, res) => {
 const port = 3000;
 
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
